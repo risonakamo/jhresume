079d14a660aedaeb49ea209399e11f9b71547201
khang4 <khang4@umbc.edu>
2017-03-21 14:39:57 -0400

    +other courses

--- courses/main.js
+++ courses/main.js
@@ -2,5 +2,11 @@ window.onload=main;
 
 function main()
 {
+    var splink=document.querySelector(".splink");
+    var spfield=document.querySelector(".spfield");
 
+    splink.addEventListener("click",function(e){
+        e.preventDefault();
+        spfield.classList.toggle("hidden");
+    });
 }
