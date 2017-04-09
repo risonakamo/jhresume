3eeee779fbcf73f94438e3ffd30367f37d533555
khang4 <khang4@umbc.edu>
2017-04-08 22:35:38 -0400

    modified redirect for use as swe index page so now it wont work as a normal page

--- redirect/main.js
+++ redirect/main.js
@@ -2,7 +2,7 @@ window.onload=main;
 
 function main()
 {
-    // setTimeout(function(){
-    //     window.location="https://risonakamo.github.io/jhresume/";
-    // },1000);
+    setTimeout(function(){
+        window.location="https://risonakamo.github.io/jhresume/";
+    },1000);
 }
